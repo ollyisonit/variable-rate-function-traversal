972e97a9ab51dd55d7c6d91cb10cb6d13c045408
dninosores <dninosores@gmail.com>
2024-03-26 00:17:15 -0700

    Expand / contract animation

--- manim/explanatory_animations.py
+++ manim/explanatory_animations.py
@@ -1,13 +1,12 @@
 from manim import *
-from math import sin
+
+AXIS_LENGTH = 8 * PI
+TIME_LENGTH = 10
 
 
-class Scene1(Scene):
+class SimpleSine(Scene):
 
     def construct(self):
-        AXIS_LENGTH = 30
-        TIME_LENGTH = 10
-
         tracker = ValueTracker(0.01)
 
         sine_axes = Axes(x_range=[0, AXIS_LENGTH, 1],
@@ -18,10 +17,12 @@ class Scene1(Scene):
                              'include_ticks': False,
                              'include_tip': False
                          }).shift(LEFT * 3)
+
         sine_graph = always_redraw(lambda: sine_axes.plot(
-            lambda x: np.sin(x), x_range=[0, tracker.get_value()]))
-        sine_dot = always_redraw(lambda: Dot(fill_color=GOLD).scale(0.5).
-                                 move_to(sine_graph.get_end()))
+            lambda x: np.sin(x), x_range=[0, tracker.get_value()]).set_color(
+                YELLOW))
+        sine_dot = always_redraw(lambda: Dot(fill_color=BLUE).scale(1).move_to(
+            sine_graph.get_end()))
 
         dot_axes = Axes(x_range=[-1, 1, 1],
                         x_length=3,
@@ -40,6 +41,42 @@ class Scene1(Scene):
                                                          buff=0.2)
 
         self.add(graph_and_dot, title)
-        self.play(tracker.animate.set_value(AXIS_LENGTH * 0.95),
+        self.play(tracker.animate.set_value(AXIS_LENGTH),
                   run_time=TIME_LENGTH,
                   rate_func=linear)
+
+
+class ExpandContract(Scene):
+
+    def construct(self):
+        tracker = ValueTracker(0.25)
+        sine_axes = Axes(x_range=[0, AXIS_LENGTH * 2, 1],
+                         x_length=5,
+                         y_range=[-2, 2, 1],
+                         y_length=3,
+                         axis_config={
+                             'include_ticks': False,
+                             'include_tip': False
+                         }).shift(LEFT * 3)
+        sine_graph = always_redraw(lambda: sine_axes.plot(
+            lambda x: np.sin(tracker.get_value() * x),
+            x_range=[0, (4 * PI) / tracker.get_value()]).set_color(YELLOW))
+
+        omega_label = always_redraw(
+            lambda: MathTex(f"\\omega = ").next_to(sine_axes, DOWN, buff=0.2))
+
+        num = DecimalNumber(0)
+        num.add_updater(lambda m: m.set_value(tracker.get_value()))
+        num.next_to(omega_label, RIGHT)
+
+        label_grp = Group(omega_label, num)
+
+        group = Group(sine_axes, sine_graph, label_grp).move_to(ORIGIN, ORIGIN)
+
+        self.add(group)
+        self.play(tracker.animate.set_value(2), run_time=2, rate_func=linear)
+        self.wait()
+        self.play(tracker.animate.set_value(0.25),
+                  run_time=2,
+                  rate_func=linear)
+        self.wait()
