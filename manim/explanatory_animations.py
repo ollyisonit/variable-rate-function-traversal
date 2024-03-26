0ab86ab4edcd14af5af087706aff68c4a6a6af46
dninosores <dninosores@gmail.com>
2024-03-26 00:44:37 -0700

    Added functions for the different solutions and started on the third animation

--- manim/explanatory_animations.py
+++ manim/explanatory_animations.py
@@ -4,6 +4,29 @@ AXIS_LENGTH = 8 * PI
 TIME_LENGTH = 10
 
 
+def omega_func(t):
+    a = 3 * PI
+    b = 5 * PI
+    f1 = 1
+    f2 = 5
+    if (t < a):
+        return f1
+    if t > b:
+        return f2
+    return ((f2 - f1) / (b - a)) * (t - a) + f1
+
+
+def bad_solution(t):
+    return np.sin(omega_func(t) * t)
+
+
+def good_solution(t):
+    # DONT FORGET TO FIX FOR FINAL EXPORT
+    d = 0.05
+    samples = [omega_func(x * d) for x in range(0, int(t / d))]
+    return np.sin(np.trapz(samples, dx=d))
+
+
 class SimpleSine(Scene):
 
     def construct(self):
@@ -80,3 +103,45 @@ class ExpandContract(Scene):
                   run_time=2,
                   rate_func=linear)
         self.wait()
+
+
+class MysteryFunction(Scene):
+
+    def construct(self):
+        tracker = ValueTracker(0.01)
+
+        sine_axes = Axes(x_range=[0, AXIS_LENGTH, 1],
+                         x_length=5,
+                         y_range=[-2, 2, 1],
+                         y_length=3,
+                         axis_config={
+                             'include_ticks': False,
+                             'include_tip': False
+                         }).shift(LEFT * 3)
+
+        sine_graph = always_redraw(lambda: sine_axes.plot(
+            lambda x: good_solution(x), x_range=[0, tracker.get_value()]).
+                                   set_color(YELLOW))
+        sine_dot = always_redraw(lambda: Dot(fill_color=BLUE).scale(1).move_to(
+            sine_graph.get_end()))
+
+        dot_axes = Axes(x_range=[-1, 1, 1],
+                        x_length=3,
+                        y_range=[-2, 2, 1],
+                        y_length=3).next_to(sine_axes, RIGHT,
+                                            buff=0.1).set_opacity(0)
+        dot = always_redraw(lambda: Dot(fill_color=BLUE).scale(5).move_to(
+            dot_axes.coords_to_point(0, good_solution(tracker.get_value()))))
+
+        graph_and_dot = Group(sine_axes, sine_graph, sine_dot, dot_axes, dot)
+
+        graph_and_dot.move_to(ORIGIN)
+
+        title = MathTex(r"f(t)=sin(\omega * t)").next_to(graph_and_dot,
+                                                         DOWN,
+                                                         buff=0.2)
+
+        self.add(graph_and_dot, title)
+        self.play(tracker.animate.set_value(AXIS_LENGTH),
+                  run_time=TIME_LENGTH,
+                  rate_func=linear)
